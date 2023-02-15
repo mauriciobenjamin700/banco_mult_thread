709effb9cf1f0ea1ed8a89ed7a4a161e76456a01
pedroVital13 <pedrinho16vital@ufpi.edu.br>
2023-02-15 11:05:41 -0300

    tela transferencia

--- Sistema_Bancario/tela_transferencia.py
+++ Sistema_Bancario/tela_transferencia.py
@@ -3,13 +3,37 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton,QLabel,QLineE
 class Tela_Transferencia(QMainWindow):
     def __init__ (self):
         super().__init__()
-        
+        """
+        uma classe para representar uma tela transferencia
+        --------
+        Atributos:
+        --------
+        esquerda: int
+        define a posição da "tela transferencia" no lado esquerdo da tela de visualização 
+        topo: int
+        define a posição da "tela transferencia" no topo da tela de vizualisação
+        largura: int
+        define a largura da "tela transferencia" na tela de visualização
+        altura: int
+        define a altura da "tela transferencia" na tela de visualização.
+        titulo: str
+        define o titulo da "tela transferencia" na tela de visualização.
+        """
         self.esquerda = 0
         self.topo = 0
         self.largura = 1460
         self.altura = 800
         self.titulo = "TELA DE TRANSFERENCIA"
 ####################################### LABEL #######################
+        '''
+        --------
+        Atributos:
+        --------
+        label1: rotula algum lugar da tela onde você define um texto, movimenta a posição, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label_valor: rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label_senha: rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label_login_recebedor: rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        '''
         self.label1 = QLabel(self)
         self.label1.setText("FAÇA SUA TRANSFERENCIA AQUI")
         self.label1.move(350, 10)
@@ -34,10 +58,16 @@ class Tela_Transferencia(QMainWindow):
         self.label_login_recebedor.resize(450,50)
         self.label_login_recebedor.setStyleSheet("QLabel {font: 40px}")
 
-        
-
-        
 ############################################### LINE EDIT ####################
+        '''
+        --------
+        Atributos:
+        --------
+        LineEdit_sacar: É uma caixa de texto onde o usuario pode adicionar o valor que deseja sacar.
+        LineEdit_senha: É uma caixa de texto onde o usuario pode adicionar a senha de login que criou para poder realizar o saque.
+        LineEdit_login_recebedor: É uma caixa de texto onde o usuario pode adicionar o login de destino que receberá a transferência.
+
+        '''
         self.LineEdit_sacar = QLineEdit(self)
         self.LineEdit_sacar.move(600, 300)
         self.LineEdit_sacar.resize(300,40)
@@ -55,7 +85,15 @@ class Tela_Transferencia(QMainWindow):
         self.LineEdit_login_recebedor.setStyleSheet('QLineEdit {font: 30px}')
 
 ############################################### QPushButton ##################
-
+        '''
+        --------
+        Atributos:
+        --------
+        botao_confirmar_transferencia: Um botão que é usado para realizar uma determinada ação que é a de executar a transferência, na criação desse botão ele pode ser nomeado, movimentado 
+        a sua posição e redimensionado de tamanho.
+        botao_voltar: Um botão que é usado para realizar uma determinada ação que é a de voltar para a tela anterior, na criação desse botão ele pode ser nomeado, movimentado 
+        a sua posição e redimensionado de tamanho.
+        '''
         self.botao_confirmar_transferencia = QPushButton('Confirmar Transferencia',self)
         self.botao_confirmar_transferencia.move(500, 620)
         self.botao_confirmar_transferencia.resize(400,60)
@@ -70,7 +108,10 @@ class Tela_Transferencia(QMainWindow):
 
 
     def Carregar_Janela (self):
-
+        '''
+        A função "Criar_Janela" cria uma janela na tela de acordo com os dados adicionado nos atributos
+        esquerdo, topo, largura, altura e titulo
+        '''
         self.setGeometry(self.esquerda, self.topo, self.largura, self.altura)
         
         self.setWindowTitle(self.titulo)
@@ -78,6 +119,9 @@ class Tela_Transferencia(QMainWindow):
         self.show()
 
 if __name__ == '__main__':
+    """
+    Em caso de teste utilizasse o " if __name__ == '__main__' "
+    """
     import sys
     aplicacao =  QApplication(sys.argv)
     j = Tela_Transferencia()
