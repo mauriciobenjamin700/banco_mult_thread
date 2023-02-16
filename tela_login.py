2b8b8e6281c0ef2b930618aa00f7559ff1070034
pedroVital13 <pedrinho16vital@ufpi.edu.br>
2023-02-16 09:41:32 -0300

    Update tela_login.py

--- tela_login.py
+++ tela_login.py
@@ -4,19 +4,21 @@ class Tela_Login(QMainWindow):
     def __init__ (self):
         """
         uma classe para representar uma tela login
-        --------
-        Atributos:
+        
+        ...
+
+        Attributes:
         --------
         esquerda: int
-        define a posição da "tela login" no lado esquerdo da tela de visualização 
+                define a posição da "tela geral" no lado esquerdo da tela de visualização 
         topo: int
-        define a posição da "tela login" no topo da tela de vizualisação
+                define a posição da "tela geral" no topo da tela de vizualisação
         largura: int
-        define a largura da "tela login" na tela de visualização
+                define a largura da "tela geral" na tela de visualização
         altura: int
-        define a altura da "tela login" na tela de visualização.
+                define a altura da "tela geral" na tela de visualização.
         titulo: str
-        define o titulo da "tela login" na tela de visualização.
+                define o titulo da "tela geral" na tela de visualização.
         """
         super().__init__()
         
@@ -27,12 +29,15 @@ class Tela_Login(QMainWindow):
         self.titulo = "TELA DE LOGIN"
 ####################################### LABEL #######################
         '''
+        
+        Attributes:
         --------
-        Atributos:
-        --------
-        label1: rotula algum lugar da tela onde você define um texto, movimenta a posição, redimensiona o tamanho da label e adiciona o tamanho da fonte.
-        label_login: rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
-        label_senha: rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label1: object
+                rotula algum lugar da tela onde você define um texto, movimenta a posição, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label_login: object
+                rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
+        label_senha: object
+                rotula algum lugar da tela onde você define um texto, movimenta a posição desse texto, redimensiona o tamanho da label e adiciona o tamanho da fonte.
         '''
         self.label1 = QLabel(self)
         self.label1.setText("FAÇA O LOGIN")
@@ -55,11 +60,13 @@ class Tela_Login(QMainWindow):
    
 ############################################### LINE EDIT ####################
         '''
+        
+        Attributes:
         --------
-        Atributos:
-        --------
-        LineEdit_login: É uma caixa de texto onde o usuario pode adicionar o usuário de login que criou para poder logar na conta.
-        LineEdit_senha: É uma caixa de texto onde o usuario pode adicionar a senha de login que criou para poder logar na conta.
+        LineEdit_login: object
+                É uma caixa de texto onde o usuario pode adicionar o usuário de login que criou para poder logar na conta.
+        LineEdit_senha: object
+                É uma caixa de texto onde o usuario pode adicionar a senha de login que criou para poder logar na conta.
 
         '''
         self.LineEdit_login = QLineEdit(self)
@@ -78,13 +85,15 @@ class Tela_Login(QMainWindow):
 
 ############################################### QPushButton ##################
         '''
+        
+        Attributes:
         --------
-        Atributos:
-        --------
-        botao_login: Um botão que é usado para realizar uma determinada ação que é a de logar e ir para a tela da conta, na criação desse botão ele pode ser nomeado, movimentado 
-        a sua posição e redimensionado de tamanho.
-        botao_voltar: Um botão que é usado para realizar uma determinada ação que é a de voltar para a tela anterior, na criação desse botão ele pode ser nomeado, movimentado 
-        a sua posição e redimensionado de tamanho.
+        botao_login: object
+                Um botão que é usado para realizar uma determinada ação que é a de logar e ir para a tela da conta, na criação desse botão ele pode ser nomeado, movimentado 
+                a sua posição e redimensionado de tamanho.
+        botao_voltar: object
+                Um botão que é usado para realizar uma determinada ação que é a de voltar para a tela anterior, na criação desse botão ele pode ser nomeado, movimentado 
+                a sua posição e redimensionado de tamanho.
         '''
         self.botao_login = QPushButton('Fazer Login',self)
         self.botao_login.move(470, 600)
