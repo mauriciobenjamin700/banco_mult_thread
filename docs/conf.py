500747d83aab06d44d3527a78af3b5fa08406cc2
pedroVital13 <pedrinho16vital@ufpi.edu.br>
2023-02-16 09:54:46 -0300

    t

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'pt-br'
+language = 'pt-BR'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
